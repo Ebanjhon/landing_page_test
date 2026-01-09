adbea6893677b24e69494c0a77f1bf64b1445e11
Êban Jhon Y <93698802+Ebanjhon@users.noreply.github.com>
2026-01-09 19:39:32 +0700

    done slick slide

--- js/script.js
+++ js/script.js
@@ -30,3 +30,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 // ======================== slick-list =====================
+$(document).ready(function () {
+  $('.single-item').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    arrows: false,
+    dots: false,
+    asNavFor: '.multiple-items'
+  });
+
+  $('.multiple-items').slick({
+    slidesToShow: 4,  
+    slidesToScroll: 1,
+    infinite: true,
+    arrows: false,
+    dots: false,
+    asNavFor: '.single-item',
+    focusOnSelect: true,
+  });
+});
