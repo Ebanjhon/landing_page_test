4d9282d0053b8121caf6c4a316cadc26b86512ac
Êban Jhon Y <93698802+Ebanjhon@users.noreply.github.com>
2026-01-09 17:53:24 +0700

    start mobile view

--- js/script.js
+++ js/script.js
@@ -1,10 +1,32 @@
 function onProductTabClick(element) {
-    console.log('Đã click vào:', element);
-    element.classList.toggle('active');
+  element.classList.toggle('active');
 }
 
 document.querySelectorAll('.product-tab-block').forEach(item => {
-    item.addEventListener('click', function () {
-        onProductTabClick(this);
-    });
+  item.addEventListener('click', function () {
+    onProductTabClick(this);
+  });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const btn = document.querySelector('.product-nutrition-info');
+  const popup = document.querySelector('.nutrition_popup-outer');
+  const content = popup.querySelector('.bg-white');
+
+  if (!btn || !popup) return;
+
+  btn.addEventListener('click', () => {
+    popup.classList.toggle('active');
+  });
+
+  popup.addEventListener('click', () => {
+    popup.classList.remove('active');
+  });
+
+  content.addEventListener('click', e => {
+    e.stopPropagation();
+  });
+});
+
+
+// ======================== slick-list =====================
