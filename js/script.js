1d6ec79eaafdefcdefaab79cac89ada4488960d1
Êban Jhon Y <93698802+Ebanjhon@users.noreply.github.com>
2026-01-12 23:05:07 +0700

    not done yet

--- js/script.js
+++ js/script.js
@@ -33,6 +33,22 @@ $(document).ready(function () {
   // show mortablele
   $('.product-table-item').on('click', initShowProductTable);
   $('.item-question').on('click', initShowAnswer);
+
+  // slick video 
+  // $('.slick-list-clip').slick({
+  //   slidesToShow: 4,
+  //   slidesToScroll: 1,
+  //   infinite: false,
+  //   arrows: false,
+  //   responsive: [
+  //     {
+  //       breakpoint: 768,
+  //       settings: {
+  //         slidesToShow: 2
+  //       }
+  //     }
+  //   ]
+  // });
 });
 
 
@@ -81,3 +97,69 @@ function initShowProductTable() {
 function initShowAnswer() {
   $(this).toggleClass('active');
 }
+
+// ========================= play video ==================
+$(function () {
+  $('.product-play').on('click', function () {
+    const currentWrapper = $(this);
+    const currentVideo = currentWrapper.find('.product-video').get(0);
+    const currentIcon = currentWrapper.find('.play-icon');
+
+    $('.product-video').each(function () {
+      if (this !== currentVideo) {
+        this.pause();
+        this.currentTime = 0;
+        $(this).siblings('.play-icon').fadeIn(200);
+      }
+    });
+
+    if (currentVideo.paused) {
+      currentVideo.muted = false;
+      currentVideo.volume = 1;
+      currentVideo.play();
+      currentIcon.fadeOut(200);
+    } else {
+      currentVideo.pause();
+      currentVideo.currentTime = 0;
+      currentIcon.fadeIn(200);
+    }
+  });
+
+  // Khi video chạy xong → reset
+  $('.product-video').on('ended', function () {
+    this.currentTime = 0;
+    this.load();
+    $(this).siblings('.play-icon').fadeIn(200);
+  });
+});
+// ===============
+$(function () {
+  const $slider = $('.slick-list-clip');
+  const $dots = $('.slick-dots li');
+
+  // Init slick
+  $slider.slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    infinite: false,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 678,
+        settings: {
+          slidesToShow: 2
+        }
+      }
+    ]
+  });
+
+  $slider.on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+    $dots.removeClass('slick-active');
+    $dots.eq(nextSlide).addClass('slick-active');
+  });
+
+  $dots.on('click', function () {
+    const index = $(this).index();
+    $slider.slick('slickGoTo', index);
+  });
+});
